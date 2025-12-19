c00d2fa354331079ac26c9edebfece9009041ade
flabarj <fla.bar.ram@gmail.com>
2025-12-19 15:49:52 -0300

    Atualiza interface portfolio

--- cards/cards-data.js
+++ cards/cards-data.js
@@ -3,28 +3,24 @@ window.PORTFOLIO_CARDS = [
     title: "Conciliação Automática",
     tag: "Comparo posição contábil vs financeira e aponto divergências.",
     image: "./assets/img/placeholders/conciliacao.png",
-    demoUrl: "#",
-    description: "Demo do projeto de conciliação automática (mini)."
+    demo: "#"
   },
   {
     title: "Fluxo de Caixa",
-    tag: "Visão de entradas/saídas, ofensores e análise gerencial.",
+    tag: "Entradas, saídas e visão gerencial.",
     image: "./assets/img/placeholders/fluxo.png",
-    demoUrl: "#",
-    description: "Demo do fluxo de caixa (mini)."
+    demo: "#"
   },
   {
     title: "Endividamento",
     tag: "Estrutura da dívida, custo, prazo e indicadores.",
     image: "./assets/img/placeholders/endividamento.png",
-    demoUrl: "#",
-    description: "Demo do endividamento (mini)."
+    demo: "#"
   },
   {
     title: "Consolidação",
-    tag: "Eliminações + participação + visão consolidada.",
+    tag: "Eliminações e visão consolidada.",
     image: "./assets/img/placeholders/Consolidacao.png",
-    demoUrl: "#",
-    description: "Demo de consolidação (mini)."
+    demo: "#"
   }
 ];
